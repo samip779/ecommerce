fe4430c483e63a736198096623a1b92dc38f676b
samip779 <samipluitel28@gmail.com>
2022-05-23 19:41:44 +0545

    Product details screen created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
     <Router>
@@ -11,6 +13,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
