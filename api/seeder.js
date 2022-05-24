4da88181e25f971a17c6047c2f4f90e35a4fe2e9
samip779 <samipluitel28@gmail.com>
2022-05-24 21:08:24 +0545

    working on seeder

---
+++ api/seeder.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {};
